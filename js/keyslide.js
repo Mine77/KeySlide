1b791e0f0539bed01703c1e45bf6df6fff66c255
Mine77 <akg@akg-no-MacBook-Pro.local>
2017-06-14 23:26:32 +0900

    bug fixed.
    Now enter only one character can be uppercase.

--- js/keyslide.js
+++ js/keyslide.js
@@ -38,7 +38,8 @@ $(function() {
         lastPos = 0,
         currentPos = 0,
         sumLength = 0,
-        firstKey = 0;
+        firstKey = 0,
+        firstKeyFlag = 0;
 
     $("#textinput").on("keydown", function(e) {
         var length = $(this).val().length;
@@ -49,6 +50,14 @@ $(function() {
             // $("#log").append("shift1<br>");
         } else if (shiftPressFlag && x[e.which] != undefined && x[e.which] != "s") {
             e.preventDefault();
+
+            if (firstKeyFlag === 0) {
+                firstKey = e.which;
+                firstKeyFlag = 1;
+            } else if (firstKeyFlag === 1) {
+                firstKeyFlag = 2;
+            }
+
             lastPos = currentPos;
             currentPos = x[e.which];
             if (currentPos < lastPos) {
@@ -62,13 +71,19 @@ $(function() {
     $("#textinput").on("keyup", function(e) {
         var length = $(this).val().length;
         if (x[e.which] === "s") {
-            $(this).val($(this).val().slice(0, length - sumLength));
+            if (firstKeyFlag === 1) {
+                $(this).val($(this).val() + String.fromCharCode(firstKey));
+                // $("#log").append(firstKey + "<br>");
+            } else {
+                $(this).val($(this).val().slice(0, length - sumLength));
+            }
             shiftDownFlag = false;
             shiftPressFlag = false;
             lastPos = 0;
             currentPos = 0;
             sumLength = 0;
             firstKey = 0;
+            firstKeyFlag = 0;
         }
     });
 });
